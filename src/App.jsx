e0c8f5da9f50bfd69894ca8ab47dc39ee3309d43
Your name <arieftaufikrahman123@gmail.com>
2024-11-20 08:45:06 +0700

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
